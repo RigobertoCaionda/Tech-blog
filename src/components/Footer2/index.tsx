fa64a0dc2a4de46b8403f7d232d9f3254708da92
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2021-12-29 16:13:25 +0100

    Adding target blank to the links

--- src/components/Footer2/index.tsx
+++ src/components/Footer2/index.tsx
@@ -7,16 +7,16 @@ import GithubIcon from '@material-ui/icons/GitHub';
 const Page = () => {
 	return (
 		<FooterArea>
-				<a href="https://linkedin.com" className="social-media">
+				<a href="https://linkedin.com" target="_blank"  rel="noreferrer" className="social-media">
 					<LinkedInIcon style={{ color: '#1b1b1b', fontSize: '2rem' }}/>
 				</a>
-				<a href="https://www.facebook.com" className="social-media">
+				<a href="https://www.facebook.com" target="_blank"  rel="noreferrer" className="social-media">
 					<FacebookIcon style={{ color: '#1b1b1b', fontSize: '2rem' }}/>
 				</a>
-				<a href="https://github.com" className="social-media">
+				<a href="https://github.com" target="_blank"  rel="noreferrer" className="social-media">
 					<GithubIcon style={{ color: '#1b1b1b', fontSize: '2rem' }}/>
 				</a>
-				<a href="https://instagram.com" className="social-media">
+				<a href="https://instagram.com" target="_blank"  rel="noreferrer" className="social-media">
 					<InstagramIcon style={{ color: '#1b1b1b', fontSize: '2rem' }}/>
 				</a>
 		</FooterArea>
